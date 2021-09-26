75267f1a2a69e951ba32c93702dca70fd9d4093a
somnathsolaskar <somnathsolaskar@10.110.143.180>
2021-09-26 19:27:54 -0400

    AED Lab 1 Assignment

---
+++ YourProfile/src/model/Person.java
@@ -0,0 +1,178 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author somnathsolaskar
+ */
+public class Person {
+        
+    private String name;
+    private String geographicData;
+    private String dob;
+    private String telNum;
+    private String faxNum;
+    private String email;
+    private String ssn;
+    private String medicalNum;
+    private String healthNum;
+    private String bankNum;
+    private String lic;
+    private String vin;
+    private String dev;
+    private String linkedIn;
+    private String ip;
+    private String uin;
+    private String photo;
+    private String biometric;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGeographicData() {
+        return geographicData;
+    }
+
+    public void setGeographicData(String geographicData) {
+        this.geographicData = geographicData;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getTelNum() {
+        return telNum;
+    }
+
+    public void setTelNum(String telNum) {
+        this.telNum = telNum;
+    }
+
+    public String getFaxNum() {
+        return faxNum;
+    }
+
+    public void setFaxNum(String faxNum) {
+        this.faxNum = faxNum;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public String getMedicalNum() {
+        return medicalNum;
+    }
+
+    public void setMedicalNum(String medicalNum) {
+        this.medicalNum = medicalNum;
+    }
+
+    public String getHealthNum() {
+        return healthNum;
+    }
+
+    public void setHealthNum(String healthNum) {
+        this.healthNum = healthNum;
+    }
+
+    public String getBankNum() {
+        return bankNum;
+    }
+
+    public void setBankNum(String bankNum) {
+        this.bankNum = bankNum;
+    }
+
+    public String getLic() {
+        return lic;
+    }
+
+    public void setLic(String lic) {
+        this.lic = lic;
+    }
+
+    public String getVin() {
+        return vin;
+    }
+
+    public void setVin(String vin) {
+        this.vin = vin;
+    }
+
+    public String getDev() {
+        return dev;
+    }
+
+    public void setDev(String dev) {
+        this.dev = dev;
+    }
+
+    public String getLinkedIn() {
+        return linkedIn;
+    }
+
+    public void setLinkedIn(String linkedIn) {
+        this.linkedIn = linkedIn;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public String getUin() {
+        return uin;
+    }
+
+    public void setUin(String uin) {
+        this.uin = uin;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public String getBiometric() {
+        return biometric;
+    }
+
+    public void setBiometric(String biometric) {
+        this.biometric = biometric;
+    }
+
+    
+}
